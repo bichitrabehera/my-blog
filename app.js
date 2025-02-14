38b359a9c22da318d01e23df0cd56e46ecb58bb4
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-02-14 19:52:52 +0530

    added vercel.json

--- app.js
+++ app.js
@@ -41,5 +41,6 @@ app.use('/', require('./server/routes/main'))
 app.use('/', require('./server/routes/admin'))
 
 
-module.exports = app;
-module.exports.handler = serverless(app);
+app.listen(PORT, () => {
+    console.log(`app listening on port http://localhost:${PORT}`);
+})
