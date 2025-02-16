ce798eaa07dc260984cdf470bfeef4b6344a6918
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-02-16 23:26:10 +0530

    styling

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 require('dotenv').config();
 const express = require('express');
-const expressLayout = require('express-ejs-layouts')
+const expressLayout = require('express-ejs-layouts');
 const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser')
-const MongoStore = require('connect-mongo')
-const session = require('express-session')
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const session = require('express-session');
 const serverless = require("serverless-http");
 
 const app = express();
 const connectDB = require('./server/config/db');
 
-//connect to db
-connectDB(); 
+// Connect to DB
+connectDB()
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cookieParser())
-app.use(methodOverride('_method'))
+app.use(cookieParser());
+app.use(methodOverride('_method'));
 
 app.use(session({
-    secret:'keyboard cat',
-    resave:false,
-    saveUninitialized:true,
-    store:MongoStore.create({
-        mongoUrl : process.env.MONGODB_URI
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
     })
-}))
+}));
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/favicon.ico', (req, res) => res.status(204).end());
 
-//template engine
-
+// Template Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
-app.use('/', require('./server/routes/main'))
-app.use('/', require('./server/routes/admin'))
+// Routes
+app.use('/', require('./server/routes/main'));
+app.use('/', require('./server/routes/admin'));
 
-
+// Export as Serverless Function for Vercel
 module.exports = app;
+module.exports.handler = serverless(app);
