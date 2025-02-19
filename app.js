b42bb61f6a066c755ba85d9d1744076a44c5b4da
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-02-19 20:19:37 +0530

    remove admin button

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const serverless = require('serverless-http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 const app = express();
 const connectDB = require('./server/config/db');
